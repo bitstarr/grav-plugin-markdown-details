007c9d6fa953a0a5b46a573546cec8b64355fdbb
Sebastian Laube <sebastian.laube@zebra.de>
2020-03-19 09:56:17 +0100

    fixing js misbehaviour

--- js/details.js
+++ js/details.js
@@ -2,8 +2,9 @@
   const collapsibles = document.querySelectorAll( '.js-details' );
   Array.prototype.forEach.call(collapsibles, function(collapsible)
   {
-    collapsible.dataset['expanded'] = false;
     let btn = collapsible.querySelector( 'button' );
+    collapsible.dataset['expanded'] = false;
+    btn.setAttribute( 'aria-expanded', false );
     btn.onclick = function()
     {
       let expanded = btn.getAttribute( 'aria-expanded' ) === 'true';
